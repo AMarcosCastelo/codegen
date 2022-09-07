87315f9379d5fc9d960a5e97396a95e443c7ee65
AMarcosCastelo <marcos-12marcos@hotmail.com>
2022-09-07 11:29:51 -0300

    feat: rename template file to templates

---
+++ test/unit/templates.spec.js
@@ -0,0 +1,57 @@
+import { jest, expect } from '@jest/globals';
+
+import {
+  repositoryTemplate,
+  serviceTemplate,
+  factoryTemplate
+} from '../../src/templates';
+import {
+  productRepositoryStub,
+  productServiceStub,
+  productFactoryStub
+} from './stubs';
+
+describe("#Codegen 3-layers arch", () => {
+  const componentName = 'product';
+  const repositoryName = `${componentName}Repository`;
+  const serviceName = `${componentName}Service`;
+
+  beforeEach(() => {
+    jest.restoreAllMocks();
+    jest.clearAllMocks();
+  });
+
+  it("#should generate repository template", () => {
+    const expected = {
+      fileName: repositoryName,
+      template: productRepositoryStub
+    };
+
+    const result = repositoryTemplate(componentName);
+
+    expect(result).toStrictEqual(expected);
+  });
+
+  it("#should generate service template", () => {
+    const expected = {
+      fileName: serviceName,
+      template: productServiceStub
+    };
+
+    const result = serviceTemplate(componentName, repositoryName);
+
+    expect(result).toStrictEqual(expected);
+  });
+
+  it("#should generate factory template", () => {
+    const factoryName = `${componentName}Factory`;
+    const expected = {
+      fileName: factoryName,
+      template: productFactoryStub
+    };
+
+    const result = factoryTemplate(componentName, repositoryName, serviceName);
+
+    expect(result).toStrictEqual(expected);
+  });
+});
